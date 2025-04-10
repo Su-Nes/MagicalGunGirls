ebc9c7868546b43e89372fa7b067f937421fcab1
IIMiKoII <81253319+IIMiKoII@users.noreply.github.com>
2025-04-10 16:20:44 +0300

    LevelSelectNew
    Dumb af changes to the level select code (still needs work, cuz I forgot how to go about setting this up in terms of design)

--- Assets/Mikus_Packs/Scripts/LevelSelect.cs
+++ Assets/Mikus_Packs/Scripts/LevelSelect.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -11,6 +12,21 @@ public class LevelSelectionTrigger : MonoBehaviour
     [SerializeField] private Button selectButton; // Button to start the level
     [SerializeField] private string levelName; // Add in inspector whichever fucking level is needed
 
+    [Header("Mission 2 Settings")]
+    [SerializeField] private Button mission2Button; // Mission 2 button
+    [SerializeField] private GameObject mission2Object; // Parent object to activate
+
+    [Header("Objects to Deactivate")]
+    [SerializeField] private List<GameObject> objectsToDeactivate; // Objects to disable
+
+    private void Awake()
+    {
+        if (mission2Button != null)
+        {
+            mission2Button.onClick.AddListener(ActivateMission2Objects);
+        }
+    }
+
     public void SelectLevel()
     {
         StartCoroutine(LevelSelectCoroutine());
@@ -20,5 +36,33 @@ public class LevelSelectionTrigger : MonoBehaviour
     {
         yield return new WaitForSeconds(1);
         SceneManager.LoadScene(levelName);
+    }
+
+    private void ActivateMission2Objects()
+    {
+        if (mission2Object != null)
+        {
+            mission2Object.SetActive(true);
+
+            foreach (Transform child in mission2Object.transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    public void DeactivateAllSelectedObjects()
+    {
+        foreach (GameObject obj in objectsToDeactivate)
+        {
+            if (obj != null)
+            {
+                obj.SetActive(false);
+                foreach (Transform child in obj.transform)
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
     }
 }
